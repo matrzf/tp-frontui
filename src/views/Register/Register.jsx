12befafb31e9d2294e6c6fbed062e0bc1dc4dd6e
Kevin <zafimiharykevin@yahoo.com>
2025-01-07 19:25:07 +0100

    update register page, register to login with useNavigate

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,9 +1,41 @@
+import { useNavigate } from "react-router-dom";
 import './register.css' 
 
 export default function Register(){
+    const navigate = useNavigate(); 
+
+    const goToLogin = () => {
+        navigate("/Login");
+    }
+
     return (
         <div className="register-container">
-            <h1>Register Page</h1>
+             <div className="header">
+                <h1>Register Page</h1> 
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <p>First Name</p>
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    <p>Last Name</p>
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    <p>Email</p>
+                    <input type="email" />
+                </div>
+                <div className="input">
+                    <p>Password</p>
+                    <input type="password" />
+                </div>
+            </div>
+            <div className="forgot-password">Lost password ? <span>Click here</span></div>
+            <div className="submit-container">
+                <div className="submit">Register</div>
+                <div className="submit" onClick={goToLogin}>Login</div>
+            </div>
         </div>
     );
     
